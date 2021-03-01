66bb818463ef68d410f076f3a9a46f79b8a92886
Boluwaji Oyewumi <boluwajioyewumi@gmail.com>
2021-03-01 14:17:14 -0600

    examples added

--- List Comprehension Start File.py
+++ List Comprehension Start File.py
@@ -48,26 +48,38 @@ print(x)
 
 
 # 2) creating a list that evaluates an expression
-
+squares = [x ** 2 for x in range(10)]
+print(squares)
 
 # Output -[0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
 
 
 # 3) creating a list from another list
+list1 = [3, 4, 5]
+multiplied = [item * 3 for item in list1]
 
+print(multiplied)
 
 # [9,12,15]
 
 
 # 4) using list comprehension for string manipulation
 
+list_of_words = ["this", "is", "a", "list", "of", "words"]
+
+items = [word[0] for word in list_of_words]
+print(items)
 
 # Output - ['t', 'i', 'a', 'l', 'o', 'w']
 
 
 # 5) Let's show how easy you can convert lower case / upper case letters.
 
+output = [x.lower() for x in ["A", "B", "C"]]
+print(output)
 
+output = [x.upper() for x in ["a", "b", "c"]]
+print(output)
 # Output 1 - ['a', 'b', 'c']
 
 
